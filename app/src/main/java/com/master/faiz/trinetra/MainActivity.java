8cc1f382b287281d56d6904b7cc9328bf547f733
isubham <subhamkumarchandrawansi@gmail.com>
2018-03-22 10:28:24 +0530

    Initial commit

---
+++ app/src/main/java/com/master/faiz/trinetra/MainActivity.java
@@ -0,0 +1,13 @@
+package com.master.faiz.trinetra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
